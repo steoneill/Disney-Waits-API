30f66ca84ea84e950a1c84cf22d3a9df35e943f0
Ste <stephen.oneill-laverty@homeagency.co.uk>
2018-02-03 23:31:58 +0000

    support for headers

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
