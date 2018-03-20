ec5e17d3aaf8edf754e0f60cb29ee036cbe1acd3
Ste <stephen.oneill-laverty@homeagency.co.uk>
2018-03-20 14:31:49 +0000

    Cleaned up arrays for React support

--- server.js
+++ server.js
@@ -1,112 +1,117 @@
-const express = require('express');
+const express = require("express");
 // include the Themeparks library
-var Themeparks = require("themeparks");
+let Themeparks = require("themeparks");
 const app = express();
-const moment = require('moment');
+const moment = require("moment");
+
+let park;
 
 // Add headers
-app.use(function (req, res, next) {
-
+app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
 });
 
-app.get('/:parkId', (req, res) => {
-
+app.get("/:parkId", (req, res) => {
   switch (req.params.parkId) {
-    case 'waltDisneyWorldMagicKingdom':
-      var park = new Themeparks.Parks.WaltDisneyWorldMagicKingdom();
+    case "WaltDisneyWorldMagicKingdom":
+      park = new Themeparks.Parks.WaltDisneyWorldMagicKingdom();
       break;
-    case 'WaltDisneyWorldEpcot':
-      var park = new Themeparks.Parks.WaltDisneyWorldEpcot();
+    case "WaltDisneyWorldEpcot":
+      park = new Themeparks.Parks.WaltDisneyWorldEpcot();
       break;
-    case 'WaltDisneyWorldHollywoodStudios':
-      var park = new Themeparks.Parks.WaltDisneyWorldHollywoodStudios();
+    case "WaltDisneyWorldHollywoodStudios":
+      park = new Themeparks.Parks.WaltDisneyWorldHollywoodStudios();
       break;
-    case 'WaltDisneyWorldAnimalKingdom':
-      var park = new Themeparks.Parks.WaltDisneyWorldAnimalKingdom();
+    case "WaltDisneyWorldAnimalKingdom":
+      park = new Themeparks.Parks.WaltDisneyWorldAnimalKingdom();
       break;
-    case 'DisneylandResortMagicKingdom':
-      var park = new Themeparks.Parks.DisneylandResortMagicKingdom();
+    case "DisneylandResortMagicKingdom":
+      park = new Themeparks.Parks.DisneylandResortMagicKingdom();
       break;
-    case 'DisneylandResortCaliforniaAdventure':
-      var park = new Themeparks.Parks.DisneylandResortCaliforniaAdventure();
+    case "DisneylandResortCaliforniaAdventure":
+      park = new Themeparks.Parks.DisneylandResortCaliforniaAdventure();
       break;
-    case 'DisneylandParisMagicKingdom':
-      var park = new Themeparks.Parks.DisneylandParisMagicKingdom();
+    case "DisneylandParisMagicKingdom":
+      park = new Themeparks.Parks.DisneylandParisMagicKingdom();
       break;
-    case 'DisneylandParisWaltDisneyStudios':
-      var park = new Themeparks.Parks.DisneylandParisWaltDisneyStudios();
+    case "DisneylandParisWaltDisneyStudios":
+      park = new Themeparks.Parks.DisneylandParisWaltDisneyStudios();
       break;
-    case 'ShanghaiDisneyResortMagicKingdom':
-      var park = new Themeparks.Parks.ShanghaiDisneyResortMagicKingdom();
+    case "ShanghaiDisneyResortMagicKingdom":
+      park = new Themeparks.Parks.ShanghaiDisneyResortMagicKingdom();
       break;
-    case 'TokyoDisneyResortMagicKingdom':
-      var park = new Themeparks.Parks.TokyoDisneyResortMagicKingdom();
+    case "TokyoDisneyResortMagicKingdom":
+      park = new Themeparks.Parks.TokyoDisneyResortMagicKingdom();
       break;
-    case 'TokyoDisneyResortDisneySea':
-      var park = new Themeparks.Parks.TokyoDisneyResortDisneySea();
+    case "TokyoDisneyResortDisneySea":
+      park = new Themeparks.Parks.TokyoDisneyResortDisneySea();
       break;
-    case 'HongKongDisneyland':
-      var park = new Themeparks.Parks.HongKongDisneyland();
+    case "HongKongDisneyland":
+      park = new Themeparks.Parks.HongKongDisneyland();
       break;
   }
 
-  // access a specific park
-  var parkInfo = {
-    parkName: park.Name.split(' -')[0],
-    parkLocation: park.Name.split(' - ')[1],
-    parkTimes: {
+  // access wait times by Promise
+  park
+    .GetWaitTimes()
+    .then(function(rides) {
+      // print each wait time
+      var Rides = rides.map(ride => {
+        parkInfo.rideInfo.push({
+          Name: ride.name,
+          waitTime: ride.waitTime,
+          fastPass: ride.fastPass,
+          status: ride.status,
+          open: ride.active,
+          openingTimes: ride.schedule
+        });
+      });
+    }, console.error)
+    .then(() => {
+      res.json(parkInfo);
+    });
 
-    },
-    rideInfo: {
-
-    }
-  };
   // get park opening times
-  park.GetOpeningTimes().then(function (times) {
-    // print opening times
-    for (var i = 0, time; time = times[i++];) {
-      parkInfo.parkTimes[time.date] = {
-        opens: time.openingTime,
-        closes: time.closingTime
-      }
-    }
+  park.GetOpeningTimes().then(function(times) {
+    var parkTimes = times.map(time => {
+      parkInfo.parkTimes.push({
+        Opens: time.openingTime,
+        Closes: time.closingTime
+      });
+    });
   }, console.error);
 
+  let ParkNameFull = park.Name.split(" - ");
 
-  // access wait times by Promise
-  park.GetWaitTimes().then(function (rides) {
-    // print each wait time
-    for (var i = 0, ride; ride = rides[i++];) {
-      parkInfo.rideInfo[ride.id] = {
-        name: ride.name,
-        waitTime: ride.waitTime,
-        fastPass: ride.fastPass,
-        status: ride.status,
-        open: ride.active,
-        openingTimes: ride.schedule
-      }
-    }
-  }, console.error).then(() => {
-    res.json(parkInfo);
-  });
-
-})
+  // access a specific park
+  var parkInfo = {
+    parkName: ParkNameFull[0],
+    parkLocation: ParkNameFull[1],
+    parkTimes: [],
+    rideInfo: []
+  };
+});
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log('server starting');
-})
+  console.log("server starting");
+});
